fdb3057723739deec696cc83b8f31bdab3fde920
jzolo22 <jzolotarev@gmail.com>
2020-12-08 23:35:01 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from './Components/Header'
+import NotesContainer from "./Containers/NotesContainer";
 
 class App extends Component {
   render() {
+    let header = "Hello World"
     return (
       <>
-        <header />
+        <Header text={header} />
+        <NotesContainer  />
       </>
     )
   }
--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Note from '../Components/Note';
+
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
+    makeLis = () => {
+        return this.apiResponse().map((note) => {
+            console.log(note)
+            return <Note note={note} />
+        })
+        
+    }
+    
     render() {
+        console.log(this)
         return (
             <ul>
-                {}
+                {this.makeLis()}
             </ul>
         )
     }
